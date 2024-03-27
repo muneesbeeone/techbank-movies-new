61ee19041516888f299202e388809f0bbce1f50d
munees <munees@techbank.finance>
2024-03-27 17:06:38 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/techbank-movies-new/', // Set the base URL to match your GitHub Pages repository path
   plugins: [vue()],
-})
+});
